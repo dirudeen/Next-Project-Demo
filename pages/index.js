ef7b1d3e9a1475fa95496b679158c4756c1e731d
Hadiru Colley <dirudeen22@gmail.com>
2023-09-13 17:47:25 +0300

    remove dummy data

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,18 @@
 import { MongoClient } from "mongodb";
 import MeetupList from "../components/meetups/MeetupList";
+import Head from "next/head";
 
-const DUMMY_MEETUPS = [
-  {
-    id: "m1",
-    title: "The first meetup",
-    address: "ALIBABA apt bahcelieveler mah, talas kayseri",
-    image:
-      "https://1.bp.blogspot.com/-_QDouVq0-98/X0JlrcU7zmI/AAAAAAAAkQw/SuYveVPa44Ib4ANn5MXjmMiOjyxsuQcywCLcBGAsYHQ/s1600/catlayana-kadar-anayurt-talas-kayseri-menu-fiyat-%2B%25284%2529.jpg",
-  },
-
-  {
-    id: "m2",
-    title: "The second meetup",
-    address: "EMNIYET sitesi bahcelieveler mah, talas kayseri",
-    image:
-      "https://projeskop.com/uploads/gallery/icon-talas-dis/Icon-talas-disgorsel-2.jpg",
-  },
-];
 
 function HomePage(props) {
-  return <MeetupList meetups={props.meetups} />;
+  return (
+    <>
+    <Head>
+      <title>React Meetups</title>
+      <meta name="description" content="Browse a huge list of highly active React meetups" />
+    </Head>
+    <MeetupList meetups={props.meetups} />;
+    </>
+  )
 }
 
 export async function getStaticProps() {
