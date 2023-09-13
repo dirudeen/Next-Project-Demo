fb9af0d8be468f0b6ff61f4ee79eb822fc6ee455
Hadiru Colley <dirudeen22@gmail.com>
2023-09-13 12:37:45 +0300

    add logic to fetch data from mongodb

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,9 +1,28 @@
+import { useRouter } from "next/router";
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 function NewMeetupPage () {
+    const router = useRouter()
 
-    const addMeetupHandler = (meetupData) => {
-        console.log(meetupData)
+    const addMeetupHandler = async (meetupData) => {
+        try {
+            const res = await fetch('api/new-meetup', {
+                method: 'POST',
+                headers: {
+                    'Content-type': "application/json"
+                },
+                body: JSON.stringify(meetupData)
+            });
+            if(!res.ok) {
+                throw new Error('something went wrong')
+            }
+            const data = await res.json();
+            console.log(data)
+        } catch (error) {
+            console.log(error.message)
+        }
+
+        router.push('/')
     }
 
     return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
